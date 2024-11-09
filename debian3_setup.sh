324cd182aa76f0ae4091fdb3ba42f6df52b721f2
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-09 16:31:55 +0800

    Update debian3_setup.sh

--- debian3_setup.sh
+++ debian3_setup.sh
@@ -1,4 +1,4 @@
-apt update -y && apt upgrade -y && apt install python3-all-dev python3-venv build-essential cmake curl wget git gh openjdk-17-jdk nano vim iproute2 procps grep libboost-all-dev gdb tmux
+apt update -y && apt upgrade -y && apt install python3-all-dev python3-venv build-essential cmake curl wget git gh openjdk-17-jdk nano vim iproute2 procps grep libboost-all-dev gdb tmux -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 -m venv myenv
 source myenv/bin/activate
