877311bcdb7a2310b651e2c72bbff57061659b74
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-12 18:40:27 +0800

    Update debian3_setup.sh

--- debian3_setup.sh
+++ debian3_setup.sh
@@ -1,4 +1,4 @@
-apt update -y && apt upgrade -y && apt install build-essential cmake python3-all-dev python3-venv openjdk-11-jdk curl wget git nano vim iproute2 procps grep libboost-all-dev gdb tmux nodejs ssh -y
+apt update -y && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus gdb git golang grep libboost-all-dev libssl-dev iproute2 make maven mc nano neovim nodejs openjdk-11-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv tar tmux vim wget -y
 wget -O gh_2.61.0_linux_arm64.deb "https://objects.githubusercontent.com/github-production-release-asset-2e65be/212613049/4bad0e61-5414-4557-9acb-3a859a55c74a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241111%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241111T100051Z&X-Amz-Expires=300&X-Amz-Signature=eb65f19fcdc2aa0276816d646cb63a65bdd7e9a3cc052211f2de4b644f693d9f&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dgh_2.61.0_linux_arm64.deb&response-content-type=application%2Foctet-stream"
 dpkg -i gh_2.61.0_linux_arm64.deb
 rm gh_2.61.0_linux_arm64.deb
