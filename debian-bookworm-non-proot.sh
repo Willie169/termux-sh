68e1cfc6eb8e47e838111a0c16084042b4e822c2
Willie Shen <cg10250207@gmail.com>
2024-11-13 12:22:02 +0800

    j

--- debian-bookworm-non-proot.sh
+++
@@ -1,2 +0,0 @@
-apt update -y && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus gdb gh git golang grep libboost-all-dev libssl-dev iproute2 make maven mc nano neovim nodejs openjdk-17-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget -y
-pip3 install numpy sympy matplotlib setuptools selenium jupyter pandas meson ninja
