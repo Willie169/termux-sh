cbdf1a3cdc498adef7cb8ab2bca611a720f8fa74
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-13 08:55:20 +0800

    Update xmrig-install.sh

--- xmrig-install.sh
+++ xmrig-install.sh
@@ -1,1 +1,1 @@
-pkg install git build-essential cmake -y && cd ~ && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/build && cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
+pkg install git build-essential cmake -y && cd ~ && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/build && cmake .. -DWITH_HWLOC=OFF && make -j$(nproc) && cd ~
