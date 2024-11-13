865d7558585e78f501ebf44ad1b49db2009e102f
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-13 08:24:50 +0800

    Update get-started.sh

--- get-started.sh
+++ get-started.sh
@@ -3,4 +3,4 @@ termux-setup-storage
 apt update -y && apt upgrade -y && pkg update -y && pkg upgrade -y
 pkg install git -y
 git clone https://github.com/Willie169/termux-sh.git
-source termux-sh/termux-setup.bash
+source ~/termux-sh/termux-setup.sh
