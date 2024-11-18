6398e901f2f40790ab59040d1e554563c80d7b63
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-18 15:09:33 +0800

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -9,7 +9,7 @@ wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh 
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2
 go install github.com/danielmiessler/fabric@latest
 npm install node-html-markdown && npm install showdown && npm install jsdom
-mkdir -p .termux && cd ~/.termux && wget https://github.com/zanjie1999/windows-fonts/raw/wine/msyh.ttc && wget https://raw.githubusercontent.com/ButTaiwan/iansui/main/fonts%2FIansui-Regular.ttf && cd ~
+mkdir -p .termux && cd ~/.termux && wget https://github.com/zanjie1999/windows-fonts/raw/wine/msyh.ttc && cd ~
 curl -sLo ~/termux-proot.sh https://github.com/Yonle/termux-proot/raw/v1.0.0/termux && chmod +x ~/termux-proot.sh && echo 'exit' | ~/termux-proot.sh
 cd ~ && mkdir debian1 && cd debian1 && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && echo 'exit' | bash debian.sh
 cd ~ && mkdir debian2 && cd debian2 && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian-xfce.sh -O debian-xfce.sh && chmod +x debian-xfce.sh && echo 'exit' | bash debian-xfce.sh
