6c800188006acb6f9918db80108b09a8f5cfb0f7
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-19 00:38:29 +0800

    Update termux-setup-all.sh

--- termux-setup-all.sh
+++ termux-setup-all.sh
@@ -5,7 +5,7 @@ apt install qemu-system-x86_64 tigervnc xfce4 -y
 cd ~ && mkdir -p .shortcuts && cp ~/termux-sh/DOTshortcuts/* ~/.shortcuts && cp ~/termux-sh/DOTbashrc ~/.bashrc && cp ~/termux-sh/DOTshortcuts/* ~
 source .bashrc && chmod +x ~/.shortcuts/*.sh && chmod +x ~/*.sh
 curl -sLo ~/termux-proot.sh https://github.com/Yonle/termux-proot/raw/v1.0.0/termux && chmod +x ~/termux-proot.sh && echo 'termux-change-repo && pkg update && pkg upgrade -y && apt update && apt upgrade -y && exit' | ~/termux-proot.sh
-sed '/allow-external-apps/s/^# //' -i ~/.termux/termux.properties && termux-reload-settings && wget 'https://github.com/termux/termux-x11/releases/download/nightly/termux-x11-nightly-1.03.01-0-all.deb' -O termux-x11-nightly-all.deb && dpkg -i termux-x11-nightly-all.deb
+sed '/allow-external-apps/s/^# //' -i ~/.termux/termux.properties && termux-reload-settings
 wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh && chmod +x setup-audio.sh && ./setup-audio.sh
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2
 go install github.com/danielmiessler/fabric@latest
