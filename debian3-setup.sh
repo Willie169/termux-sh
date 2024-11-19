83130f9a62841acf150a7180f86437cce99fcbbd
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-19 08:39:05 +0800

    Update debian3-setup.sh

--- debian3-setup.sh
+++ debian3-setup.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb git golang grep libboost-all-dev libssl-dev iproute2 make maven mc nano neovim nodejs openjdk-11-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv ruby tar tmux vim wget -y
+apt update -y && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb git golang grep libboost-all-dev libssl-dev iproute2 make maven mc nano neovim nodejs openjdk-11-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv ruby tar tmux vim wget -y
 wget 'https://github.com/cli/cli/releases/download/v2.61.0/gh_2.61.0_linux_arm64.deb'
 dpkg -i gh_2.61.0_linux_arm64.deb
 rm gh_2.61.0_linux_arm64.deb
