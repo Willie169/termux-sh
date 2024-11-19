549e30e92997a6ed8eb163f9349a555a4e8c5ea4
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-19 17:12:44 +0800

    Update termux-setup-all.sh

--- termux-setup-all.sh
+++ termux-setup-all.sh
@@ -13,8 +13,6 @@ cd ~ && mkdir debian1 && cd debian1 && wget https://raw.githubusercontent.com/An
 cd ~ && mkdir debian3 && cd debian3 && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && echo 'exit' | bash debian.sh
 cd ~ && proot-distro install debian
 cd ~ && ./termux-sh/proot-install-debianbox.sh
-cd ~ && cat ~/termux-sh/debian-bookworm.sh <(echo '
-exit') | ./proot-debian.sh
-cd ~ && cat ~/termux-sh/box64-wine64-winetricks.sh <(echo '
-exit') | ./proot-debianbox.sh
+cd ~ && cat ~/termux-sh/debian-bookworm.sh <(echo 'exit') | ./proot-debian.sh
+cd ~ && cat ~/termux-sh/box64-wine64-winetricks.sh <(echo 'exit') | ./proot-debianbox.sh
 cd ~
