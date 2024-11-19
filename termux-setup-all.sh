b89ea005c790dbad0e53b0de428779315d773734
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-19 12:03:24 +0800

    Update termux-setup-all.sh

--- termux-setup-all.sh
+++ termux-setup-all.sh
@@ -14,10 +14,10 @@ cd ~ && mkdir debian3 && cd debian3 && wget https://raw.githubusercontent.com/An
 cd ~ && cp ~/termux-sh/debian1-setup.sh ~/debian1/debian-fs/root && cp ~/termux-sh/debian3-setup.sh ~/debian3/debian-fs/root
 cd ~ && proot-distro install debian
 cd ~ && ./termux-sh/proot-install-debian01.sh
-cd ~/debian1 && echo 'chmod +x debian1-setup.sh && ./debian1-setup.sh && rm debian1-setup.sh && exit' | ./start-debian.sh
-cd ~/debian3 && echo 'chmod +x debian3-setup.sh && ./debian3-setup.sh && rm debian3-setup.sh && exit' | ./start-debian.sh
 cd ~ && cat ~/termux-sh/debian-bookworm.sh ~/termux-sh/box64-wine64-winetricks.sh <(echo 'exit') | ./proot-debian.sh
 cd ~ && echo 'apt update && apt upgrade -y' | ./proot-debian01.sh
+cd ~/debian1 && echo 'chmod +x debian1-setup.sh && ./debian1-setup.sh && rm debian1-setup.sh && exit' | ./start-debian.sh
+cd ~/debian3 && echo 'chmod +x debian3-setup.sh && ./debian3-setup.sh && rm debian3-setup.sh && exit' | ./start-debian.sh
 cd ~ && mkdir debian2 && cd debian2 && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian-xfce.sh -O debian-xfce.sh && echo 'apt update && apt upgrade -y
 vncserver-stop
 trap "exit" INT TERM
