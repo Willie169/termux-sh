6f74621cc7118205388c62c96181fbba91f21603
Willie169 <cg10250207@gmail.com>
2024-11-19 13:11:07 +0800

    l

--- get-started.sh
+++
@@ -1,7 +0,0 @@
-cd ~
-termux-setup-storage
-termux-change-repo
-pkg update && pkg upgrade -y && apt update && apt upgrade -y && pkg install git -y
-git clone https://github.com/Willie169/termux-sh.git
-chmod +x ~/termux-sh/*.sh
-source ~/termux-sh/termux-setup-all.sh
