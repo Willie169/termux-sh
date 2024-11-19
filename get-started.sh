bbcc3c51aaa3accd7a5fffe809311805a6aaaa58
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-19 08:02:01 +0800

    Update get-started.sh

--- get-started.sh
+++ get-started.sh
@@ -3,5 +3,5 @@ termux-setup-storage
 termux-change-repo
 pkg update && pkg upgrade -y && apt update && apt upgrade -y && pkg install git -y
 git clone https://github.com/Willie169/termux-sh.git
-chmod +x ~/termux-sh/*.sh
+chmod +x ~/termux-sh/*.sh && chmod +x ~/DOTshortcuts/*.sh
 source ~/termux-sh/termux-setup-all.sh
