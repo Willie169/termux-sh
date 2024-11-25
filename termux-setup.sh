ced0001969ae276fbe57857bfbcf99042408d975
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-25 11:43:51 +0800

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,15 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
-cd ~ && wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh && bash setup-audio.sh
-cd ~ && go install github.com/danielmiessler/fabric@latest
-npm install node-html-markdown && npm install showdown && npm install jsdom
-proot-distro install debian
-proot-distro install ubuntu
-bash ~/termux-sh/proot-install-debianbox.sh
-cat ~/termux-sh/debian-bookworm.sh <(echo -e "\nexit") | bash ~/proot-debian.sh
-cat ~/termux-sh/ubuntu-24-04.sh <(echo -e "\nexit") | bash ~/proot-ubuntu.sh
-cat ~/termux-sh/box64-wine64-winetricks.sh <(echo -e "\nexit") | bash ~/proot-debianbox.sh
-cp ~/termux-sh/debian1-setup.sh ~/debian1/debian-fs/root && echo 'bash debian1-setup.sh && rm debian1-setup.sh && exit' bash ~/debian1.sh
-cp ~/termux-sh/debian2-setup.sh ~/debian2/debian-fs/root && echo 'bash debian1-setup.sh && rm debian1-setup.sh && exit' bash ~/debian2.sh
-cd ~
 echo 'termux-setup.sh finished'
 curl parrot.live
