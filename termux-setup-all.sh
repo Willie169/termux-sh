3d39ebc82934aae715ac1888c974264f84040694
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-11-25 11:33:53 +0800

    Update termux-setup-all.sh

--- termux-setup-all.sh
+++ termux-setup-all.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg update && pkg install tmux -y
+pkg update && pkg install curl wget proot tar tmux -y
 tmux new-session -d -s 'termux-setup-all'
 tmux split-window -h
 tmux send-keys -t 0 'bash ~/termux-sh/termux-setup.sh' C-m
