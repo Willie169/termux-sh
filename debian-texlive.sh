ff5a6a968f91195a61065691fb9c3bfd499b2aa3
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-02 11:27:46 +0800

    Update debian-texlive.sh

--- debian-texlive.sh
+++ debian-texlive.sh
@@ -1,3 +1,3 @@
-apt update --allow-releaseinfo-change -y && apt upgrade -y && apt install texlive-full nano vim -y
+apt update --allow-releaseinfo-change -y && apt upgrade -y && apt install mediawiki2latex nano pandoc texlive-full -y
 rm -rf .bash_profile
 exit
