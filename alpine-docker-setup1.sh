5cd816bbe88782984459f5363cf2c31904e19ad4
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-17 11:27:55 +0800

    Update alpine-docker-setup1.sh

--- alpine-docker-setup1.sh
+++ alpine-docker-setup1.sh
@@ -4,8 +4,7 @@ auto eth0
 iface eth0 inet dhcp' >> /etc/network/interfaces
 /etc/init.d/networking restart
 echo 'https://dl-cdn.alpinelinux.org/alpine/v3.21/community
-https://dl-cdn.alpinelinux.org/alpine/v3.21/main
-https://dl-cdn.alpinelinux.org/alpine/v3.21/releases' >> /etc/apk/repositories
+https://dl-cdn.alpinelinux.org/alpine/v3.21/main' >> /etc/apk/repositories
 apk update
 apk upgrade
 echo 'KEYMAPOPTS="tr"
@@ -17,8 +16,7 @@ iface eth0 inet dhcp"
 TIMEZONEOPTS="-z UTC"
 PROXYOPTS="none"
 APKREPOSOPTS="https://dl-cdn.alpinelinux.org/alpine/v3.21/community
-https://dl-cdn.alpinelinux.org/alpine/v3.21/main
-https://dl-cdn.alpinelinux.org/alpine/v3.21/releases"
+https://dl-cdn.alpinelinux.org/alpine/v3.21/main"
 SSHDOPTS="-c openssh"
 NTPOPTS="-c busybox"
 DISKOPTS="-v -m sys -s 0 /dev/sda"' >> answerfile
