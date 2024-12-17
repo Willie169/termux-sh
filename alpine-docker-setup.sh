f078f03aa7539a9f6cdb116f922cac581346c4aa
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-17 12:21:55 +0800

    Update alpine-docker-setup.sh

--- alpine-docker-setup.sh
+++ alpine-docker-setup.sh
@@ -1,19 +1,6 @@
-echo 'KEYMAPOPTS="tr"
-HOSTNAMEOPTS="-n alpine"
-INTERFACESOPTS="auto lo
-iface lo inet loopback
-auto eth0
-iface eth0 inet dhcp"
-TIMEZONEOPTS="-z UTC"
-PROXYOPTS="none"
-APKREPOSOPTS="https://dl-cdn.alpinelinux.org/alpine/v3.21/community
-https://dl-cdn.alpinelinux.org/alpine/v3.21/main"
-SSHDOPTS="-c openssh"
-NTPOPTS="-c busybox"
-DISKOPTS="-v -m sys -s 0 /dev/sda"' >> answerfile
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-apk update
+apk update && apk upgrade
 apk add docker
 service docker start
 rc-update add docker
