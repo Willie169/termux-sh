15e2fbec725ca636e4284db73b2bfcaffddecde7
Willie169 <cg10250207@gmail.com>
2024-12-17 11:23:31 +0800

    j

--- alpine-docker-setup1.sh
+++ alpine-docker-setup1.sh
@@ -3,9 +3,9 @@ iface lo inet loopback
 auto eth0
 iface eth0 inet dhcp' >> /etc/network/interfaces
 /etc/init.d/networking restart
-echo 'https://mirror.twds.com.tw/alpine/v3.21/community
-https://mirror.twds.com.tw/alpine/v3.21/main
-https://mirror.twds.com.tw/alpine/v3.21/releases' >> /etc/apk/repositories
+echo 'https://dl-cdn.alpinelinux.org/alpine/v3.21/community
+https://dl-cdn.alpinelinux.org/alpine/v3.21/main
+https://dl-cdn.alpinelinux.org/alpine/v3.21/releases' >> /etc/apk/repositories
 apk update
 apk upgrade
 echo 'KEYMAPOPTS="tr"
@@ -16,9 +16,9 @@ auto eth0
 iface eth0 inet dhcp"
 TIMEZONEOPTS="-z UTC"
 PROXYOPTS="none"
-APKREPOSOPTS="https://mirror.twds.com.tw/alpine/v3.21/community
-https://mirror.twds.com.tw/alpine/v3.21/main
-https://mirror.twds.com.tw/alpine/v3.21/releases"
+APKREPOSOPTS="https://dl-cdn.alpinelinux.org/alpine/v3.21/community
+https://dl-cdn.alpinelinux.org/alpine/v3.21/main
+https://dl-cdn.alpinelinux.org/alpine/v3.21/releases"
 SSHDOPTS="-c openssh"
 NTPOPTS="-c busybox"
 DISKOPTS="-v -m sys -s 0 /dev/sda"' >> answerfile
