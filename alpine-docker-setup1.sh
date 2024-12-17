01f83808bfdf0b53a8cd45594ca7e5f8278f6847
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-17 11:40:46 +0800

    Update alpine-docker-setup1.sh

--- alpine-docker-setup1.sh
+++ alpine-docker-setup1.sh
@@ -11,3 +11,10 @@ https://dl-cdn.alpinelinux.org/alpine/v3.21/main"
 SSHDOPTS="-c openssh"
 NTPOPTS="-c busybox"
 DISKOPTS="-v -m sys -s 0 /dev/sda"' >> answerfile
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+apk update
+apk add docker
+service docker start
+rc-update add docker
+docker run hello-world
