5592729b955264e6ac05bfd3eed1746ccae1ce55
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-23 17:20:13 +0800

    Update qemu-debian-arm64.sh

--- DOTshortcuts/qemu-debian-arm64.sh
+++ DOTshortcuts/qemu-debian-arm64.sh
@@ -1,3 +1,3 @@
 memory='1024'
 cpu='2'
-qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a53 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic
+qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a72 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic
