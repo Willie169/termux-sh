cd1fcf2363314a228d7423d0deb8c3be0dbe0239
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-23 16:49:45 +0800

    Update qemu-debian-amd64-setup.sh

--- qemu-debian-amd64-setup.sh
+++ qemu-debian-amd64-setup.sh
@@ -1,7 +1,7 @@
 size='5G'
 memory='1024'
 cpu='2'
-pkg update && pkg install qemu-utils qemu-common qemu-system-x86_64-headless wget -y
+pkg update && pkg install qemu-utils qemu-common qemu-system-x86-64-headless wget -y
 mkdir ~/debian-amd64 && cd ~/debian-amd64
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2
 qemu-img resize debian-12-nocloud-amd64.qcow2 +$size && qemu-system-x86_64 -machine q35 -m $memory -smp cpus=$cpu -cpu qemu64 -drive file=~/debian-amd64/debian-12-nocloud-amd64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic
