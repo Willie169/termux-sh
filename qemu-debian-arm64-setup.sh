3c4bcb97bbc462092ad7a0e869433384a2ee9feb
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-23 16:43:01 +0800

    Update qemu-debian-arm64-setup.sh

--- qemu-debian-arm64-setup.sh
+++ qemu-debian-arm64-setup.sh
@@ -4,5 +4,4 @@ cpu='2'
 pkg update && pkg install qemu-utils qemu-common qemu-system-x86_64-headless wget -y
 mkdir ~/debian-arm64 && cd ~/debian-arm64
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-arm64.qcow2
-qemu-img resize debian-12-nocloud-arm64.qcow2 +$size
-qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a53 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic
+qemu-img resize debian-12-nocloud-arm64.qcow2 +$size && qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a53 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic
