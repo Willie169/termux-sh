c95ba063c14019e224a23a81f11045876e50691a
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-23 18:31:52 +0800

    Update qemu-debian-amd64-vnc.sh

--- DOTshortcuts/qemu-debian-amd64-vnc.sh
+++ DOTshortcuts/qemu-debian-amd64-vnc.sh
@@ -1,3 +1,3 @@
 memory='1024'
 cpu='2'
-qemu-system-x86_64 -machine q35 -m $memory -smp cpus=$cpu -cpu qemu64 -drive file=~/debian-amd64/debian-12-nocloud-amd64.qcow2,format=qcow2 -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -vnc :0
+qemu-system-x86_64 -machine q35 -m $memory -smp cpus=$cpu -cpu qemu64 -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -drive file=~/debian-amd64/debian-12-nocloud-amd64.qcow2,format=qcow2 -vnc :0
