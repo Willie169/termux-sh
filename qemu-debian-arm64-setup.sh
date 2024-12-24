0e81479a1ec6cb0f447ac4eacc53a201c2e8a7b5
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-24 13:57:36 +0800

    Update qemu-debian-arm64-setup.sh

--- qemu-debian-arm64-setup.sh
+++ qemu-debian-arm64-setup.sh
@@ -5,4 +5,4 @@ pkg update && pkg install qemu-utils qemu-common qemu-system-aarch64-headless wg
 mkdir ~/debian-arm64 && cd ~/debian-arm64
 wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-arm64.qcow2
 qemu-img resize debian-12-nocloud-arm64.qcow2 +$size
-qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu host -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-aarch64-code.fd -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -nographic
+qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a72 -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-aarch64-code.fd -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -drive file=~/debian-arm64/debian-12-nocloud-arm64.qcow2,format=qcow2 -nographic
