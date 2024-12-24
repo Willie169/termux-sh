98c50f26a0159985af90cd6d0888f25c6e549167
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-24 13:57:17 +0800

    Update qemu-alpine-aarch64-setup.sh

--- qemu-alpine-aarch64-setup.sh
+++ qemu-alpine-aarch64-setup.sh
@@ -5,4 +5,4 @@ pkg update && pkg install qemu-utils qemu-common qemu-system-aarch64-headless wg
 mkdir ~/alpine-aarch64 && cd ~/alpine-aarch64
 wget https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/aarch64/alpine-virt-3.21.0-aarch64.iso
 qemu-img create -f qcow2 alpine-aarch64.img $size
-qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu host -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-aarch64-code.fd -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.21.0-aarch64.iso -drive file=alpine-aarch64.img,format=qcow2 -nographic
+qemu-system-aarch64 -machine virt -m $memory -smp cpus=$cpu -cpu cortex-a72 -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-aarch64-code.fd -netdev user,id=n1,dns=8.8.8.8,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.21.0-aarch64.iso -drive file=alpine-aarch64.img,format=qcow2 -nographic
