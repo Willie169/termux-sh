77c48a835343e1f8110099976872c032113cc02d
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-26 20:29:12 +0800

    Update debian-texlive.sh

--- debian-texlive.sh
+++ debian-texlive.sh
@@ -1,3 +1,8 @@
-apt update --allow-releaseinfo-change -y && apt upgrade -y && apt install nano pandoc texlive-full wget -y
+apt update --allow-releaseinfo-change -y && apt upgrade -y && apt install curl nano pandoc texlive-full wget -y
+mkdir -p ~/.font
+curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansCJK-VF.otf.ttc --output ~/.font/NotoSansCJK-VF.otf.ttc
+curl https://raw.githubusercontent.com/stipub/stixfonts/master/fonts/static_otf/STIXTwoMath-Regular.otf --output ~/.font/STIXTwoMath-Regular.otf
+https://github.com/$1/$2/releases/tag/$3/$4
+fc-cache -fv
 rm -rf .bash_profile
 exit
