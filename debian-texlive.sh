0ba58f3b16a2a42eaa7a9b05e0e9964103ca995f
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-26 21:03:23 +0800

    Update debian-texlive.sh

--- debian-texlive.sh
+++ debian-texlive.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.font
 curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansCJK-VF.otf.ttc --output ~/.font/NotoSansCJK-VF.otf.ttc
 curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansMonoCJK-VF.otf.ttc --output ~/.font/NotoSansMonoCJK-VF.otf.ttc
 curl https://raw.githubusercontent.com/stipub/stixfonts/master/fonts/static_otf/STIXTwoMath-Regular.otf --output ~/.font/STIXTwoMath-Regular.otf
+fc-cache -fv
 export alias src='source'
 export FONTCONFIG_PATH=$HOME/.font
 source .bashrc
