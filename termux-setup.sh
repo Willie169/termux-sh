2d6cf64b61f3270fbdaae3b4d3ee4a1224ae2e45
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-26 23:20:20 +0800

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg upgrade -y && apt update && apt upgrade -y
 pkg install android-tools apksigner automake bash build-essential bzip2 clang cmake command-not-found curl dbus debootstrap dpkg fdroidcl ffmpeg file fontconfig-utils fontconfig freetype gdb gh git glab-cli golang grep iproute2 jq make maven mc nano ncurses-utils neovim nodejs openjdk-17-x openjdk-17 openssh-sftp-server openssh openssl-tool openssl perl procps proot proot-distro python-ensurepip-wheels python-pip python ruby rust tar termux-am-socket termux-am termux-api termux-auth termux-exec termux-keyring termux-licenses termux-tools tigervnc tmux tor torsocks vim wget which x11-repo xfce4 zsh -y && pkg install termux-x11-nightly -y
-mkdir -p ~/.shortcuts && cp ~/termux-sh/DOTshortcuts/* ~/.shortcuts && cp ~/termux-sh/DOTshortcuts/* ~ && chmod +x ~/.shortcuts/* && chmod +x ~/*.sh && mv ~/bashrc.sh ~/.bashrc && source ~/.bashrc
+mkdir -p ~/.shortcuts && cp ~/termux-sh/DOTshortcuts/* ~/.shortcuts && cp ~/termux-sh/DOTshortcuts/* ~ && chmod +x ~/.shortcuts/* && chmod +x ~/*.sh && cp ~/bashrc.sh ~/.bashrc && source ~/.bashrc
 sed '/allow-external-apps/s/^# //' -i ~/.termux/termux.properties && termux-reload-settings
 echo 'termux-change-repo && pkg update && pkg upgrade -y && apt update && apt upgrade -y && exit' | bash ~/proot-termux.sh
 cd ~ && wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh && bash setup-audio.sh
