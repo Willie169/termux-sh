bd6e24441e6f0fb0ea2db990c48dd7a944d1c9d7
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-26 21:17:15 +0800

    Update debian-texlive.sh

--- debian-texlive.sh
+++ debian-texlive.sh
@@ -1,8 +1,8 @@
 apt update --allow-releaseinfo-change -y && apt upgrade -y && apt install curl nano pandoc texlive-full -y
-mkdir -p ~/.font
-curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansCJK-VF.otf.ttc --output ~/.font/NotoSansCJK-VF.otf.ttc
-curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansMonoCJK-VF.otf.ttc --output ~/.font/NotoSansMonoCJK-VF.otf.ttc
-curl https://raw.githubusercontent.com/stipub/stixfonts/master/fonts/static_otf/STIXTwoMath-Regular.otf --output ~/.font/STIXTwoMath-Regular.otf
+mkdir -p ~/.fonts
+curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansCJK-VF.otf.ttc --output ~/.fonts/NotoSansCJK-VF.otf.ttc
+curl https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/OTC/NotoSansMonoCJK-VF.otf.ttc --output ~/.fonts/NotoSansMonoCJK-VF.otf.ttc
+curl https://raw.githubusercontent.com/stipub/stixfonts/master/fonts/static_otf/STIXTwoMath-Regular.otf --output ~/.fonts/STIXTwoMath-Regular.otf
 fc-cache -fv
 export alias src='source'
 export FONTCONFIG_PATH=$HOME/.font
