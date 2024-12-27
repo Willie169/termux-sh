d85170238612255a92527c2649e173e1897fb779
Willie169 <133617026+Willie169@users.noreply.github.com>
2024-12-27 09:38:15 +0800

    Update debian-bookworm.sh

--- debian-bookworm.sh
+++ debian-bookworm.sh
@@ -5,7 +5,9 @@ source ~/.bashrc
 python3 -m venv .env
 source .env/bin/activate
 pip3 install numpy sympy matplotlib setuptools selenium jupyter pandas meson ninja
+mkdir -p /usr/share/fonts/opentype/stix
 curl https://raw.githubusercontent.com/stipub/stixfonts/master/fonts/static_otf/STIXTwoMath-Regular.otf --output /usr/share/fonts/opentype/stix/STIXTwoMath-Regular.otf
+mkdir -p /usr/share/fonts/truetype/noto
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTC/NotoSansCJK-Thin.ttc --output /usr/share/fonts/truetype/noto/NotoSansCJK-Thin.ttc
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTC/NotoSansCJK-DemiLight.ttc --output /usr/share/fonts/truetype/noto/NotoSansCJK-DemiLight.ttc
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTC/NotoSansCJK-Light.ttc --output /usr/share/fonts/truetype/noto/NotoSansCJK-Light.ttc
@@ -20,7 +22,17 @@ curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Serif/OTC/NotoSer
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Serif/OTC/NotoSerifCJK-SemiBold.ttc --output /usr/share/fonts/truetype/noto/NotoSerifCJK-SemiBold.ttc
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Serif/OTC/NotoSerifCJK-Bold.ttc --output /usr/share/fonts/truetype/noto/NotoSerifCJK-Bold.ttc
 curl https://raw.githubusercontent.com/notofonts/noto-cjk/main/Serif/OTC/NotoSerifCJK-Black.ttc --output /usr/share/fonts/truetype/noto/NotoSerifCJK-Black.ttc
-curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKtc-Regular.otf
+mkdir -p /usr/share/fonts/opentype/noto
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKtc-Regular.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKtc-Regular.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKtc-Bold.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKtc-Bold.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKsc-Regular.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKsc-Regular.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKsc-Bold.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKsc-Bold.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKjp-Regular.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKjp-Regular.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKjp-Bold.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKjp-Bold.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKkr-Regular.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKkr-Regular.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKkr-Bold.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKkr-Bold.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKhk-Regular.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKhk-Regular.otf
+curl https://raw.githubusercontent.com/notofonts/noto-cjk/tree/main/Sans/Mono/NotoSansMonoCJKhk-Bold.otf --output /usr/share/fonts/opentype/noto/NotoSansMonoCJKhk-Bold.otf
 fc-cache -fv
 fc-list | grep "STIX Two Math"
 fc-list | grep "Noto Sans CJK"
