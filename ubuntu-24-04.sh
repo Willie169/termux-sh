c196b7b33d4970fdf220827f1c9c43f02d82904a
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-01-26 01:12:28 +0800

    Update ubuntu-24-04.sh

--- ubuntu-24-04.sh
+++ ubuntu-24-04.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh git golang grep libboost-all-dev libssl-dev iproute2 jq make maven mc nano neovim nodejs openjdk-17-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget zsh -y
+apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh git golang grep libboost-all-dev libssl-dev iproute2 jq make maven mc nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl perl procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget zsh -y
 python3 -m venv myenv
 source myenv/bin/activate
 pip3 install numpy sympy matplotlib setuptools selenium jupyter pandas meson ninja
