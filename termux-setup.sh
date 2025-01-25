8344b14d4f940344cfde95a7283460417732109d
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-01-26 01:31:51 +0800

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -14,5 +14,5 @@ bash ~/termux-sh/proot-install-debianbox.sh
 cat ~/termux-sh/debian-bookworm.sh <(echo -e "\nexit") | bash ~/proot-debian.sh
 cat ~/termux-sh/ubuntu-24-04.sh <(echo -e "\nexit") | bash ~/proot-ubuntu.sh
 cat ~/termux-sh/box64-wine64-winetricks.sh <(echo -e "\nexit") | bash ~/proot-debianbox.sh
-cp ~/termux-sh/debian-dev.sh ~/debian1/debian-fs/root/.bash_profile && bash ~/debian1.sh
+cp ~/termux-sh/debian-buster.sh ~/debian1/debian-fs/root/.bash_profile && bash ~/debian1.sh
 mkdir ~/debian2 && cp ~/termux-sh/debian-xfce-mod.sh ~/debian2 && cd ~/debian2 && bash debian-xfce-mod.sh --after "$(printf "apt update --allow-releaseinfo-change -y && apt --fix-broken install -y && apt upgrade -y && apt install curl -y\necho \"alias exit='vncserver-stop && trap '' INT TERM && builtin exit'\" >> ~/.bashrc && source ~/.bashrc\nrm -rf ~/.bash_profile\nvncserver-stop && trap '' INT TERM\necho 'termux-setup-all.sh finished'\ncurl parrot.live")"
