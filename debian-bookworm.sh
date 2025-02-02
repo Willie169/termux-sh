fecbf53af1c4f8ab11370755b09774aa063ec9fe
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-02-02 15:19:52 +0800

    Update debian-bookworm.sh

--- debian-bookworm.sh
+++ debian-bookworm.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh git golang grep libboost-all-dev libssl-dev iproute2 jq make maven mc nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl pandoc perl procps python3-pip python3-all-dev python3-venv rust-all tar texlive-full tmux vim wget zsh -y
+apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh ghostscript git golang grep libboost-all-dev libssl-dev iproute2 jq make maven mc nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl pandoc perl procps python3-pip python3-all-dev python3-venv rust-all tar texlive-full tmux vim wget zsh -y
 echo 'alias src="source"
 export FONTCONFIG_PATH="/usr/share/fonts"' >> ~/.bashrc
 source ~/.bashrc
