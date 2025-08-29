ab076c6a56887538bc948d377a0eda3bc144d443
Willie Shen <cg10250207@gmail.com>
2025-08-29 19:20:42 +0800

    u

--- debian-bookworm.sh
+++ debian-bookworm.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh ghostscript git golang grep ffmpeg libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc mlocate nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl pandoc perl perl-doc procps python3-pip python3-all-dev python3-venv rust-all sudo tar tmux vim wget zsh -y
+apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh ghostscript git golang gpg grep ffmpeg libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc mlocate nano neovim openjdk-17-jdk openssh-client openssh-server openssl pandoc perl perl-doc pipx procps python3-pip python3-all-dev python3-venv rust-all sudo tar tmux vim wget zsh -y
 cd ~
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xvzf install-tl-unx.tar.gz
@@ -6,7 +6,7 @@ rm install-tl-unx.tar.gz
 cd install-tl-*
 perl install-tl --no-interaction
 cd ~
-echo 'export PATH=/usr/local/texlive/2025/bin/aarch64-linux:$PATH' >> ~/.bashrc
+echo 'export PATH="$PATH:/usr/local/texlive/2025/bin/aarch64-linux"' >> ~/.bashrc
 source ~/.bashrc
 rm -rf install-tl-*
 mkdir -p ~/.config/fontconfig/conf.d
@@ -17,10 +17,102 @@ cat > ~/.config/fontconfig/conf.d/99-texlive.conf << EOF
   <dir>/usr/local/texlive/2025/texmf-dist/fonts</dir>
 </fontconfig>
 EOF
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+corepack enable yarn
+corepack enable pnpm
+npm install -g jsdom marked marked-gfm-heading-id node-html-markdown showdown
+pipx install poetry
 python3 -m venv .env
 source .env/bin/activate
 pip3 install jupyter librosa matplotlib meson ninja numpy pandas pydub scipy selenium setuptools sympy
-mkdir /usr/share/fonts/opentype/xits
+deactivate
+rm -f ~/.bashrc
+cat > ~/.bashrc << EOF
+# ~/.bashrc: executed by bash(1) for non-login shells.
+
+# Note: PS1 and umask are already set in /etc/profile. You should not
+# need this unless you want different defaults for root.
+# PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
+# umask 022
+
+# You may uncomment the following lines if you want `ls' to be colorized:
+# export LS_OPTIONS='--color=auto'
+# eval "$(dircolors)"
+# alias ls='ls $LS_OPTIONS'
+# alias ll='ls $LS_OPTIONS -l'
+# alias l='ls $LS_OPTIONS -lA'
+#
+# Some more alias to avoid making mistakes:
+# alias rm='rm -i'
+# alias cp='cp -i'
+# alias mv='mv -i'
+
+export PATH="$PATH:/usr/local/texlive/2025/bin/aarch64-linux"
+export GOROOT="$PREFIX/lib/go"
+export GOPATH="$HOME/go"
+export KIT="/usr/share/LaTeX-ToolKit"
+export PATCH="/root/texmf/tex/latex/physics-patch"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias src='source'
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+
+gpull() {
+    level="${1:-0}"
+    if [ "$level" -eq 0 ]; then
+        repo_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+        if [ -n "$repo_dir" ]; then
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        else
+            echo "Not in a Git repo."
+        fi
+    else
+        depth=$((level + 1))
+        find . -mindepth "$depth" -maxdepth "$depth" -type d -name .git | while read -r gitdir; do
+            repo_dir=$(dirname "$gitdir")
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        done
+    fi
+}
+
+gacp() {
+    git add .
+    git commit -m "$1"
+    git push
+}
+
+gtr() {
+    git tag -a v"$1" -m "Version $1 release"
+    git push origin v"$1"
+    gh release create v"$1" --title "Version $1 release" --notes ''
+}
+
+updatetex() {
+    dir=$(pwd)
+    cd /usr/share/LaTeX-ToolKit
+    sudo git pull
+    cd ~/texmf/tex/latex/physics-patch
+    git pull
+    cd dir
+}
+
+EOF
+source ~/.bashrc
+rm -f ~/.vimrc
+cat > ~/.vimrc << EOF
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+set autoindent
+EOF
+mkdir -p /usr/share/fonts/opentype/xits
 cd /usr/share/fonts/opentype/xits
 wget https://github.com/aliftype/xits/releases/download/v1.302/XITS-1.302.zip
 unzip XITS-1.302.zip
@@ -28,7 +120,7 @@ cd XITS-1.302
 mv *.otf ..
 cd ..
 rm -rf XITS-1.302*
-mkdir /usr/share/fonts/noto-cjk
+mkdir -p /usr/share/fonts/noto-cjk
 cd /usr/share/fonts/noto-cjk
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/SubsetOTF/TC/NotoSansTC-Thin.otf
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/SubsetOTF/TC/NotoSansTC-Regular.otf
@@ -111,3 +203,12 @@ wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSan
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Regular.otf
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Bold.otf
 fc-cache -fv
+cd ~
+mkdir -p texmf
+cd texmf
+mkdir -p tex
+cd tex
+mkdir -p latex
+cd latex
+git clone https://github.com/Willie169/physics-patch
+cd ~
--- ubuntu-24-04.sh
+++ ubuntu-24-04.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh ghostscript git golang grep ffmpeg libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc mlocate nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl pandoc perl perl-doc procps python3-pip python3-all-dev python3-venv rust-all sudo tar tmux vim wget zsh -y
+apt update && apt upgrade -y && apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus file gdb gh ghostscript git golang gpg grep ffmpeg libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc nano neovim openjdk-17-jdk openssh-client openssh-server openssl pandoc perl perl-doc pipx procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget zsh -y
 cd ~
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xvzf install-tl-unx.tar.gz
@@ -6,8 +6,6 @@ rm install-tl-unx.tar.gz
 cd install-tl-*
 perl install-tl --no-interaction
 cd ~
-echo 'export PATH=/usr/local/texlive/2025/bin/aarch64-linux:$PATH' >> ~/.bashrc
-source ~/.bashrc
 rm -rf install-tl-*
 mkdir -p ~/.config/fontconfig/conf.d
 cat > ~/.config/fontconfig/conf.d/99-texlive.conf << EOF
@@ -17,10 +15,183 @@ cat > ~/.config/fontconfig/conf.d/99-texlive.conf << EOF
   <dir>/usr/local/texlive/2025/texmf-dist/fonts</dir>
 </fontconfig>
 EOF
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+corepack enable yarn
+corepack enable pnpm
+npm install -g jsdom marked marked-gfm-heading-id node-html-markdown showdown
+pipx install poetry
 python3 -m venv .env
 source .env/bin/activate
 pip3 install jupyter librosa matplotlib meson ninja numpy pandas pydub scipy selenium setuptools sympy
-mkdir /usr/share/fonts/opentype/xits
+deactivate
+rm -f ~/.bashrc
+cat > ~/.bashrc << EOF
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+HISTCONTROL=ignoredups:ignorespace
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+#if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+#    . /etc/bash_completion
+#fi
+
+export PATH="$PATH:/usr/local/texlive/2025/bin/aarch64-linux"
+export GOROOT="$GOROOT:$PREFIX/lib/go"
+export GOPATH="$GOPATH:$HOME/go"
+export KIT="/usr/share/LaTeX-ToolKit"
+export PATCH="/root/texmf/tex/latex/physics-patch"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias src='source'
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+
+gpull() {
+    level="${1:-0}"
+    if [ "$level" -eq 0 ]; then
+        repo_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+        if [ -n "$repo_dir" ]; then
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        else
+            echo "Not in a Git repo."
+        fi
+    else
+        depth=$((level + 1))
+        find . -mindepth "$depth" -maxdepth "$depth" -type d -name .git | while read -r gitdir; do
+            repo_dir=$(dirname "$gitdir")
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        done
+    fi
+}
+
+gacp() {
+    git add .
+    git commit -m "$1"
+    git push
+}
+
+gtr() {
+    git tag -a v"$1" -m "Version $1 release"
+    git push origin v"$1"
+    gh release create v"$1" --title "Version $1 release" --notes ''
+}
+
+updatetex() {
+    dir=$(pwd)
+    cd /usr/share/LaTeX-ToolKit
+    sudo git pull
+    cd ~/texmf/tex/latex/physics-patch
+    git pull
+    cd dir
+}
+
+EOF
+source ~/.bashrc
+rm -f ~/.vimrc
+cat > ~/.vimrc << EOF
+set tabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+set autoindent
+EOF
+mkdir -p /usr/share/fonts/opentype/xits
 cd /usr/share/fonts/opentype/xits
 wget https://github.com/aliftype/xits/releases/download/v1.302/XITS-1.302.zip
 unzip XITS-1.302.zip
@@ -28,7 +199,7 @@ cd XITS-1.302
 mv *.otf ..
 cd ..
 rm -rf XITS-1.302*
-mkdir /usr/share/fonts/noto-cjk
+mkdir -p /usr/share/fonts/noto-cjk
 cd /usr/share/fonts/noto-cjk
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/SubsetOTF/TC/NotoSansTC-Thin.otf
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/SubsetOTF/TC/NotoSansTC-Regular.otf
@@ -111,3 +282,12 @@ wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSan
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Regular.otf
 wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Bold.otf
 fc-cache -fv
+cd ~
+mkdir -p texmf
+cd texmf
+mkdir -p tex
+cd tex
+mkdir -p latex
+cd latex
+git clone https://github.com/Willie169/physics-patch
+cd ~
