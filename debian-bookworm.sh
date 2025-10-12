95512842215f9090eaef371617ee4010ca700f4b
Willie Shen <cg10250207@gmail.com>
2025-10-12 15:47:02 +0800

    m

--- debian-bookworm.sh
+++ debian-bookworm.sh
@@ -33,6 +33,7 @@ deactivate
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh && echo "set mouse=a
 set signcolumn=no
 set foldcolumn=0
+set nolinebreak
 
 nnoremap <leader>k :if &mouse ==# 'a' \| set mouse= \| else \| set mouse=a \| endif<CR>
 " | tee ~/.vim_runtime/my_configs.vim > /dev/null
