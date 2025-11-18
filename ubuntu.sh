a1d1d365d80068b73c3a277f599ae3b9011e5247
Willie Shen <cg10250207@gmail.com>
2025-11-18 20:32:02 +0800

    a

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,14 @@ cd ~
 apt update
 apt upgrade -y
 apt install alsa-utils aptitude autoconf automake bash bison build-essential bzip2 clang cmake command-not-found curl dbus dbus-x11 default-jdk dnsutils ffmpeg file flex gcc gdb gh ghostscript git gnucobol golang gperf gpg grep g++ iproute2 iverilog libboost-all-dev libeigen3-dev libgsl-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev libssl-dev jpegoptim jq make maven mc nano neovim net-tools openssh-client openssh-server openssl optipng pandoc perl perl-doc pipx pulseaudio-utils procps python3-pip python3-all-dev python3-venv rust-all tar tigervnc-standalone-server tmux tree unrar valgrind verilator vim wget xfce4 xfce4-goodies xfce4-terminal x11-utils x11-xserver-utils zsh -y
+wget -q https://sourceforge.net/projects/sdl-bgi/files/SDL2_bgi-3.0.4.tar.gz/download -O SDL2_bgi-3.0.4.tar.gz
+tar -xzf SDL2_bgi-3.0.4.tar.gz
+cd SDL2_bgi-3.0.4
+./mkpkg.sh
+cd build
+dpkg -i sdl2_bgi_3.0.4-1_arm64.deb
+cd ../..
+rm -rf SDL2_bgi-3.0.4 SDL2_bgi-3.0.4.tar.gz
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/; s/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 mkdir -p /run/sshd
 chmod 755 /run/sshd
@@ -159,7 +167,7 @@ alias src='source'
 alias sshd='/usr/sbin/sshd'
 alias g++20='g++ -std=gnu++20'
 alias g++202='g++ -std=gnu++20 -O2'
-alias g++SDL='g++ -lSDL_bgi -lSDL2 -lm'
+alias g++SDL='g++ -std=gnu++20 -lm -lSDL2 -lSDL_bgi'
 
 gh-latest() {
     curl -s "https://api.github.com/repos/$1/releases/latest" | jq -r ".assets[].browser_download_url | select(test(\"$(printf '%s' "$2" | sed -e 's/\./\\\\./g' -e 's/\*/.*/g')\"))" | xargs curl -L -O
