952bc841d33041b0846332e89d2b309482c8e581
Willie Shen <cg10250207@gmail.com>
2026-02-24 10:52:01 +0800

    l

--- DOTshortcuts/xmrig.sh
+++ DOTshortcuts/xmrig.sh
@@ -9,4 +9,4 @@ tor -f .tor/torrc &
 TORPID=$!
 trap 'kill $TORPID; rm -r ~/xmrig/build/.tor' EXIT
 until echo > /dev/tcp/127.0.0.1/9054 2>/dev/null; do sleep 1; done
-./xmrig -o pool.supportxmr.com:3333 -u 48j6iQDeCSDeH46gw4dPJnMsa6TQzPa6WJaYbBS9JJucKqg9Mkt5EDe9nSkES3b8u7V6XJfL8neAPAtbEpmV2f4XC7bdbkv -k -t $(nproc) --cpu-priority=0 --nicehash -x 127.0.0.1:9054
+./xmrig -o pool.supportxmr.com:3333 -u 48j6iQDeCSDeH46gw4dPJnMsa6TQzPa6WJaYbBS9JJucKqg9Mkt5EDe9nSkES3b8u7V6XJfL8neAPAtbEpmV2f4XC7bdbkv -k -x 127.0.0.1:9054 -t $(nproc) --cpu-priority=0
