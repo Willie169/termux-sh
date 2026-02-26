e03d631f41ec6aa5cec23b3100ca5300ce3f3df7
Willie Shen <cg10250207@gmail.com>
2026-02-26 16:42:14 +0800

    l

--- xmrig-install.sh
+++ xmrig-install.sh
@@ -1,6 +1,6 @@
 cd ~
 pkg install git build-essential cmake luv openssl tor -y
-git clone https://github.com/xmrig/xmrig.git
+git clone https://github.com/Willie169/xmrig.git
 mkdir ~/xmrig/build
 cd ~/xmrig/build
 cmake .. -DWITH_HWLOC=OFF
