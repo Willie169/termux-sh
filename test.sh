ac1ebe41937ecd21a14a6da2b50247b9079bbd76
Willie169 <cg10250207@gmail.com>
2026-06-21 16:47:43 +0800

    y

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd ~
+pkg update
+pkg install git perl -y
 git clone https://github.com/Willie169/termux-sh.git
 cd termux-sh
 perl -i -pe 's/^DEBIANINSTALL=0/DEBIANINSTALL=1/;
@@ -8,4 +10,4 @@ s/^( *)(pkg|pip|npm|proot-distro)/$1\/entrypoint.sh $2/;
 s/(\&\&|\|\|) (pkg|pip|npm|proot-distro)/$1 \/entrypoint.sh $2/;
 s/pkg install (\$PKG|xfce4 -y)/pkg install $1 -s/;
 s/\.\/ubuntu-debian.sh/.\/ubuntu-debian.sh --test/' termux-setup.sh
-bash ~/termux-sh/termux-setup.sh
+./termux-setup.sh
