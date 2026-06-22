8876a430e0eecb7a00656bb43bfb6e86df51979a
Willie169 <cg10250207@gmail.com>
2026-06-22 15:29:11 +0800

    a

--- ubuntu-debian.sh
+++ ubuntu-debian.sh
@@ -107,7 +107,7 @@ cd ~ || exit
 corepack enable yarn
 npm i jsdom markdown-toc marked marked-gfm-heading-id node-html-markdown showdown
 npm i -g --allow-scripts=opencode-ai bash-language-server dockerfile-language-server-nodejs http-server opencode-ai pyright @linthtml/linthtml @openai/codex
-pipx install cmake-language-server gh2md img2pdf jupyterlab jupytext libretranslate meson notebook poetry pylatexenc tldr uv xmljson yamllint
+pipx install cmake-language-server gh2md img2pdf jupyterlab jupytext libretranslate meson notebook pylatexenc tldr uv xmljson yamllint
 wget --tries=100 --retry-connrefused --waitretry=5 https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
 bash Miniforge3-Linux-aarch64.sh -b -p "${HOME}/conda"
 rm Miniforge3-Linux-aarch64.sh
