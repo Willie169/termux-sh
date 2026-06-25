4df9c1294297ed3a94fe07f6e96e1841b21d2248
Willie Shen <cg10250207@gmail.com>
2026-06-25 21:41:58 +0800

    a

--- termux-setup.sh
+++ termux-setup.sh
@@ -138,14 +138,15 @@ gh_latest -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' krist
 # and
 # + tar -xf -
 # tar: chown 501:20 'superhtml': Operation not permitted
+# other tar similarly
 xz -dc aarch64-linux.tar.xz | tar -xf - || true
 rm aarch64-linux.tar.xz*
 mv superhtml ~/.local/bin
 mkdir eclipse.jdt.ls
 cd eclipse.jdt.ls || exit
 wget --tries=100 --retry-connrefused --waitretry=5 'https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.57.0/jdt-language-server-1.57.0-202602261110.tar.gz'
-tar -xzf 'download.php?file=%2Fjdtls%2Fmilestones%2F1.57.0%2Fjdt-language-server-1.57.0-202602261110.tar.gz'
-rm 'download.php?file=%2Fjdtls%2Fmilestones%2F1.57.0%2Fjdt-language-server-1.57.0-202602261110.tar.gz'
+tar -xzf 'download.php?file=%2Fjdtls%2Fmilestones%2F1.57.0%2Fjdt-language-server-1.57.0-202602261110.tar.gz' || true
+rm 'download.php?file=%2Fjdtls%2Fmilestones%2F1.57.0%2Fjdt-language-server-1.57.0-202602261110.tar.gz'*
 cd ~ || exit
 if [ "$GITDELTA" -ne 0 ]; then
 pkg install git-delta -y || true
