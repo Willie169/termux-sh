675366be6c10e0f8d0dbef152ce003ab92be4898
Willie Shen <cg10250207@gmail.com>
2026-07-20 19:19:41 +0800

    u

--- test-setup.sh
+++ test-setup.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg update >/dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive pkg install proot proot-distro -y -o Dpkg::Options::="--force-confnew" >/dev/null 2>&1
-proot-distro install ubuntu:latest --name ubuntu >/dev/null 2>&1
-cp ~/termux-sh/test.sh "${PREFIX}/var/lib/proot-distro/containers/ubuntu/rootfs/root/" >/dev/null 2>&1
-echo './test.sh' | bash <(proot-distro login "$UBUNTU" --redirect-ports --shared-tmp --isolated --get-proot-cmd)
+proot-distro install ubuntu:latest --name test >/dev/null 2>&1
+cp ~/termux-sh/test.sh "${PREFIX}/var/lib/proot-distro/containers/test/rootfs/root/" >/dev/null 2>&1
+echo './test.sh' | bash <(proot-distro login test --redirect-ports --shared-tmp --isolated --get-proot-cmd)
 exit
