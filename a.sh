1b1a369a6a25083fbdb70fcde4896949d6cbcfdb
Willie Shen <cg10250207@gmail.com>
2026-07-20 22:05:25 +0800

    y

--- a.sh
+++
@@ -1,5 +0,0 @@
-ln -sf ${PREFIX:-/usr}/bin/bash ${TMPDIR:-/tmp}/bash-link1
-for i in $(seq 1 10); do
-ln -sf ${TMPDIR:-/tmp}/bash-link$i ${TMPDIR:-/tmp}/bash-link$(( i + 1 ))
-${TMPDIR:-/tmp}/bash-link$(( i + 1 )) --version >/dev/null
-done
--- test-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive pkg install proot proot-distro -y -o Dpkg::Options::="--force-confnew" >/dev/null 2>&1
-proot-distro install ubuntu:latest --name test >/dev/null 2>&1
-cp ~/termux-sh/test.sh "${PREFIX}/var/lib/proot-distro/containers/test/rootfs/root/" >/dev/null 2>&1
-echo './test.sh' | bash <(proot-distro login test --redirect-ports --shared-tmp --isolated --get-proot-cmd)
-exit
