37843ac204f306987e735cda6af964a26897e282
Willie Shen <cg10250207@gmail.com>
2026-08-11 23:16:28 +0800

    2026-08-11T15:16:28+00:00

--- DOTshortcuts/proot-debianbox.sh
+++ DOTshortcuts/proot-debianbox.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-for f in ~/.bashrc.d/*-functions.sh; do
+for f in ~/.bashrc.d/bashrc.d/*-functions.sh; do
 	test -f "$f" && source "$f"
 done
 [ -z "$PROOT_DEBIANBOX" ] && test -f ~/.bashrc.proot && source ~/.bashrc.proot
