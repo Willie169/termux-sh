af89a2187d2fba89c52e446821cde748fdc93807
Willie Shen <cg10250207@gmail.com>
2026-08-23 16:01:08 +0800

    2026-08-23T08:01:08+00:00

--- disable-phantom-process-killer-adb.sh
+++ disable-phantom-process-killer-adb.sh
@@ -3,4 +3,4 @@
 echo 'device_config set_sync_disabled_for_tests persistent
 device_config put activity_manager max_phantom_processes 2147483647
 settings put global settings_enable_monitor_phantom_procs false
-' | adb shell
+' | adb "$@" shell
--- persistent-notifications-adb.sh
+++ persistent-notifications-adb.sh
@@ -7,4 +7,4 @@ done
 for pkg in $(pm list packages --user 10 -f | sed "s/.*=//"); do
   appops set --user 10 "$pkg" SYSTEM_EXEMPT_FROM_DISMISSIBLE_NOTIFICATIONS allow
 done
-' | adb shell
+' | adb "$@" shell
