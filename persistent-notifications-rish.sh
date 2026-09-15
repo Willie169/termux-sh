381a347e66a45f4caa9b011d76632b57bcbef622
Willie Shen <cg10250207@gmail.com>
2026-09-15 20:36:07 +0800

    2026-09-15T12:36:07+00:00

--- persistent-notifications-rish.sh
+++ persistent-notifications-rish.sh
@@ -2,9 +2,9 @@
 
 # shellcheck disable=2016
 echo 'while IFS= read -r line; do
-forpkg in $(pm list packages --user "$line" -f | sed "s/.*=//"); do
-  appops set --user 0 "$pkg" SYSTEM_EXEMPT_FROM_DISMISSIBLE_NOTIFICATIONS allow
-done
+    for pkg in $(pm list packages --user "$line" -f | sed "s/.*=//"); do
+      appops set --user "$line" "$pkg" SYSTEM_EXEMPT_FROM_DISMISSIBLE_NOTIFICATIONS allow
+    done
 done <<EOF
 $(pm list users | tail -n+2 | sed '"'"'s/^.*UserInfo{//; s/:.*}.*$//'"'"')
 EOF
