05e4058b9eb1ac8759450575de83780d7ee4fb7f
Willie Shen <cg10250207@gmail.com>
2026-09-26 20:33:26 +0800

    2026-09-26T12:33:26+00:00

--- ubuntu-debian.sh
+++ ubuntu-debian.sh
@@ -84,8 +84,6 @@ DEBIAN_FRONTEND=noninteractive apt install xclip xfce4 xfce4-goodies xinit -y -o
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 . "$HOME/.cargo/env"
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-echo 'yes' | cpan
-cpan -i App::cpanminus
 # shellcheck disable=2155
 export UBUNTU_VERSION_ID=$(
   if grep -q '^NAME="Linux Mint"' /etc/os-release; then
